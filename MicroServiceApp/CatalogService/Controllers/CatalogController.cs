d7dce8b4673c34e4bd120cf1352ee4a277bf1414
Sachin Kumar <sachin.kumar06@nagarro.com>
2020-06-28 22:17:16 +0530

    Initial Check-in microservice and api gateway ocelot

---
+++ MicroServiceApp/CatalogService/Controllers/CatalogController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CatalogService.DataBase;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CatalogService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CatalogController : ControllerBase
+    {
+        private readonly DatabaseContext _db;
+
+        public CatalogController(DatabaseContext databaseContext)
+        {
+            _db = databaseContext;
+        }
+
+        [HttpGet("product")]
+        public async Task<IActionResult> GetProducts()
+        {
+            var product = await _db.Products.ToListAsync();
+            if (product == null) return NotFound();
+            return Ok(product);
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetCategory()
+        {
+            var categories = await _db.Categories.ToListAsync();
+            if (categories == null) return NotFound();
+            return Ok(categories);
+        }
+    }
+}
---
+++ MicroServiceApp/CatalogService/DataBase/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CatalogService.DataBase
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int UnitPrice { get; set; }
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+    }
+}
